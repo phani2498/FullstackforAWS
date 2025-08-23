a019adb3c5921f8cd95c743c9cc041e2152e0a6c
phani2498 <phaneendra2017@gmail.com>
2025-08-23 10:50:08 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://13.61.120.35:9090/springapp1';
+const BASE_URL = 'https://51.21.105.226:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
